7a445c31022f5a375bbc2dcb7aaeb4401bd0682e
lealalves <leealalves@hotmail.com>
2022-11-03 14:45:10 -0300

    fork

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
-});
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
