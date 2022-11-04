5350c22ea4b352f74b6692655841e1ae72bf5079
lealalves <leealalves@hotmail.com>
2022-11-03 21:41:21 -0300

    challenge completed

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 })
 
+app.get('/api/whoami', (req, res) => {
+  res.send({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['sec-ch-ua']
+  })
+})
+
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
